49f95ccdc3c9002617760be1eab9106c442f9801
peter <pngarland@gmail.com>
2013-06-04 13:20:18 -0700

    heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 HasManyThrough::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.default_url_options = { :host => 'glacial-mountain-3501.herokuapp.com/' }
 
   # Code is not reloaded between requests
   config.cache_classes = true
